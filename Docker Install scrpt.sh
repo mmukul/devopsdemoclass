72ef7b492836a4809ab48145eeeb10dfb0357d26
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-05 13:57:05 +0530

    Update Docker Install scrpt.sh

--- Docker Install scrpt.sh
+++ Docker Install scrpt.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 
-## Install Docker
+## Install Docker-ce
 sudo yum install -y yum-utils && \
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yum && \
 sudo systemctl start docker && \
 sudo systemctl enable docker
