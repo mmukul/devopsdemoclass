78c521455d8f24ca3a972e884841f6f24a89a3d5
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-05 13:51:48 +0530

    Update Install_Jenkins.sh

--- Install_Jenkins.sh
+++ Install_Jenkins.sh
@@ -8,15 +8,3 @@ sudo yum install jenkins -y
  
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
 sudo firewall-cmd --reload
-
-## Install Docker
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yum
-sudo systemctl start docker
-sudo systemctl enable docker
-
-## Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
